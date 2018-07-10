3fd49b3f1d6e4869a2b635e3816739cfbafed40c
Abdelhakim AKODADI <abdelhakim.akodadi@inria.fr>
2018-07-10 15:54:38 +0200

    Add login activity

--- app/src/main/java/com/example/obelmo/androiddemo/MainActivity.java
+++ app/src/main/java/com/example/obelmo/androiddemo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.obelmo.androiddemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button clickMeButton = findViewById(R.id.click_me_button);
+        final TextView helloTextView = findViewById(R.id.hello_text_view);
+
+        final Intent intent = new Intent(this, LoginActivity.class);
+
+        clickMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
     }
 }
